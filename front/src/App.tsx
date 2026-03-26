08ad067ca26384b5b4eed0a0cf70e55e140b8b60
The-NumeN <stivegamy@outlook.fr>
2026-03-26 14:54:18 +0100

    Initial project setup

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main className="app-shell">
+      <section className="hero">
+        <p className="eyebrow">React</p>
+        <h1>Squelette initial du projet</h1>
+        <p className="description">
+          Page de Test
+        </p>
+      </section>
+    </main>
+  );
+}
+
+export default App;
